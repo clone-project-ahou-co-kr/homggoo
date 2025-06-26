0611af2e68b8b75f7b14906722031feb526607dd
yoonjunghyeon <90305884+yoonjunghyeon@users.noreply.github.com>
2025-06-26 09:23:59 +0900

    product shoot!!
    hi

--- src/main/resources/static/assets/scripts/product-index.js
+++ src/main/resources/static/assets/scripts/product-index.js
@@ -1,43 +1,45 @@
-let curPos = 0; // 현재 보여주는 이미지의 인덱스 번호
-let positionValue = 0; // 이미지 태그의 위치 값 지정할 변수
-const IMAGE_WIDTH = 1455;
+/* Overflow toggle */
+const hideOverflow = document.getElementById("overflow");
 
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-const images = document.querySelector(".images");
+hideOverflow.addEventListener("click", () => {
+    document.querySelector(".container").style.overflow = hideOverflow.checked
+        ? "hidden"
+        : "visible";
+});
 
-// 다음 이미지로 이동하는 함수
-function next(){
-    // 현재 이미지가 마지막 이미지가 아닌 경우에만 이동 가능
-    if(curPos < 3){
-        prevBtn.removeAttribute('disabled'); // 이전 버튼 활성화
-        positionValue -= IMAGE_WIDTH; // 다음 이미지 위치로 이동
-        images.style.transform = `translateX(${positionValue}px)`; // 이미지 이동 애니메이션
-        curPos += 1; // 현재 이미지 인덱스 증가
-    }
-    if(curPos === 3){
-        nextBtn.setAttribute('disabled','true');
-    }
-}
+/* Carousel */
+const items = document.querySelector(".items");
+const dots = document.querySelectorAll(".dot");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const total = items.children.length - 1;
+const active = "active";
+let current = 0;
 
-// 이전 이미지로 이동하는 함수
-function prev(){
-    if(curPos > 0){
-        nextBtn.removeAttribute('disabled'); // 다음 버튼 활성화
-        positionValue += IMAGE_WIDTH; // 이전 이미지 위치로 이동
-        images.style.transform = `translateX(${positionValue}px)`; // 이미지 이동 애니메이션
-        curPos -= 1; // 현재 이미지 인덱스 감소
-    }
-    if(curPos === 0){
-        prevBtn.setAttribute('disabled','true');
-    }
-}
+const setActiveDot = () => {
+    dots.forEach((button, i) => {
+        i === current
+            ? button.classList.add(active)
+            : button.classList.remove(active);
+    });
+};
 
+const scrollToCurrent = () => {
+    items.style.transform = `translateX(${current * -100}%`;
+    setActiveDot();
+};
 
-function init(){
-    prevBtn.setAttribute('disabled','true'); // 페이지 로드 시 이전 버튼 비활성화
-    prevBtn.addEventListener("click", prev); // 이전 버튼에 클릭 이벤트 추가
-    nextBtn.addEventListener("click", next); // 다음 버튼에 클릭 이벤트 추가
-}
+const scrollPrev = () => {
+    if (current === 0) return;
+    current--;
+    scrollToCurrent();
+};
 
-init(); // 초기화 함수 호출
+const scrollNext = () => {
+    if (current === total) return;
+    current++;
+    scrollToCurrent();
+};
+
+prev.addEventListener("click", scrollPrev);
+next.addEventListener("click", scrollNext);
