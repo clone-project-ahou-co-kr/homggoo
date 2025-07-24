f1c48b31c235be9a76a3285afbfd1626d7bd28cc
jiwon <jjiioonnee6978@gmail.net>
2025-07-24 11:20:07 +0900

    .

--- src/main/java/com/hgc/homggoo/services/comment/CommentService.java
+++ src/main/java/com/hgc/homggoo/services/comment/CommentService.java
@@ -2,12 +2,15 @@ package com.hgc.homggoo.services.comment;
 
 import com.hgc.homggoo.entities.article.ArticleEntity;
 import com.hgc.homggoo.entities.comment.CommentEntity;
+import com.hgc.homggoo.entities.notification.NotificationEntity;
 import com.hgc.homggoo.entities.user.UserEntity;
 import com.hgc.homggoo.mappers.article.ArticleMapper;
 import com.hgc.homggoo.mappers.comment.CommentMapper;
+import com.hgc.homggoo.mappers.notification.NotificationMapper;
 import com.hgc.homggoo.results.CommonResult;
 import com.hgc.homggoo.results.ResultTuple;
 import com.hgc.homggoo.results.Results;
+import com.hgc.homggoo.vos.ArticleVo;
 import com.hgc.homggoo.vos.CommentVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,11 +21,13 @@ import java.time.LocalDateTime;
 public class CommentService {
     private final ArticleMapper articleMapper;
     private final CommentMapper commentMapper;
+    private final NotificationMapper notificationMapper;
 
     @Autowired
-    public CommentService(CommentMapper commentMapper, ArticleMapper articleMapper) {
+    public CommentService(CommentMapper commentMapper, ArticleMapper articleMapper, NotificationMapper notificationMapper) {
         this.commentMapper = commentMapper;
         this.articleMapper = articleMapper;
+        this.notificationMapper = notificationMapper;
     }
 
     public Results insert(CommentEntity comment, UserEntity user) {
@@ -39,6 +44,19 @@ public class CommentService {
         comment.setCommentId(comment.getCommentId());
         comment.setDeleted(false);
         comment.setCreatedAt(LocalDateTime.now());
+
+        ArticleVo article = this.articleMapper.selectById(comment.getArticleId());
+
+        if (article != null && !article.getUserEmail().equals(user.getEmail())) {
+            // 자신 글에 단 댓글은 알림 제외
+            NotificationEntity notification = NotificationEntity.builder()
+                    .receiverEmail(article.getUserEmail())
+                    .articleId(article.getId())
+                    .createdAt(LocalDateTime.now())
+                    .type("comment")
+                    .build();
+            this.notificationMapper.insert(notification);
+        }
 
         return this.commentMapper.insert(comment) > 0 ? CommonResult.SUCCESS : CommonResult.FAILURE;
     }
